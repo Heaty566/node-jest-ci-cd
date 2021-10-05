d852e3d737a0f1c65de7ac630960077a4ab682ce
hetay566 <heaty566@gmail.com>
2021-10-05 14:22:36 +0700

    Change something

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 export const testFunction = () => {
-        return "v1.0.0";
+        return "v1.0.1";
 };
 
 app.get("/test", (req, res) => {
